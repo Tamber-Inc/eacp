48879fa611c126ea3ca8c44f1ecd5f7f357df14c
Eyal Amir <eyalamirmusic@gmail.com>
2026-05-08 17:20:14 -0700

    Improved rpc example and added client/server

---
+++ Apps/HttpRpcDemo/Server.cpp
@@ -0,0 +1,28 @@
+#include <eacp/Core/Threads/EventLoop.h>
+#include <eacp/Network/HTTPRpc/RpcServer.h>
+#include <eacp/Network/HTTPServer/HttpServer.h>
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    auto port = 8088;
+    if (argc > 1)
+        port = std::atoi(argv[1]);
+
+    auto httpServer = eacp::HTTP::Server {};
+    auto rpc = eacp::HTTP::Rpc::Server {httpServer};
+    rpc.useStaticRegistry();
+
+    if (! httpServer.listen(port))
+    {
+        std::cerr << "Failed to bind RPC server on port " << port << "\n";
+        return 1;
+    }
+
+    std::cout << "RPC listening on http://localhost:" << port
+              << "/rpc (Ctrl-C to quit)\n";
+
+    eacp::Threads::runEventLoop();
+    return 0;
+}
--- Apps/WebViewEmbed/Main.cpp
+++ Apps/WebViewEmbed/Main.cpp
@@ -1,16 +1,7 @@
-#include <eacp/Network/HTTPRpc/RpcServer.h>
-#include <eacp/Network/HTTPServer/HttpServer.h>
 #include <eacp/WebView/WebView.h>
-
-#include <iostream>
 
 using namespace eacp;
 using namespace Graphics;
-
-namespace
-{
-constexpr auto kRpcPort = 8088;
-}
 
 struct MyApp
 {
@@ -20,20 +11,11 @@ struct MyApp
         window.setContentView(webView);
 
         bridge.useStaticRegistry();
-        rpc.useStaticRegistry();
-
-        if (httpServer.listen(kRpcPort))
-            std::cout << "RPC listening on http://localhost:" << kRpcPort
-                      << "/rpc\n";
-        else
-            std::cerr << "Failed to bind RPC server on port " << kRpcPort << "\n";
     }
 
     WebView webView {embeddedOptions("WebApp")};
     WebViewBridge bridge {webView};
     Window window;
-    HTTP::Server httpServer;
-    HTTP::Rpc::Server rpc {httpServer};
 };
 
 int main()
